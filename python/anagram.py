4f7fbc852451f61d180fc129f078230c4566c91f
Nabil Rahman <mdnabiltech@gmail.com>
2025-10-05 17:13:26 -0400

    Remove unwanted python files/folders

--- python/anagram.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-a = input("Give me a word: ").upper
-b = input("Give me a word: ").upper
-
-ax = Counter(a)
-bx = Counter(b)
-
-print(f"Output: {ax == bx}")
--- python/fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 51):
-    out = ""
-    if i % 3 == 0:
-        out += "Fizz"
-    if i % 5 == 0:
-        out += "Buzz"
-    if out == "":
-        out += str(i)
-    print(out)
